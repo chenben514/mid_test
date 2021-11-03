e94d50497dd84bdc96628a189fdb7c7c9c24c611
chenben514 <86956296+chenben514@users.noreply.github.com>
2021-11-03 14:52:16 +0800

    Create store_hist.js

---
+++ js/store_hist.js
@@ -0,0 +1,11 @@
+let scoreHist = document.querySelector(".score_hist");
+const category = document.querySelector("#category").value;
+const level = document.querySelector("#level");
+scoreHist.innerText = "";
+for (i = 0; i < level.length; i++) {
+    // scoreHist.innerText = scoreHist.innerText + category + "_" + level[i].innerText + ": 00\n"
+    scoreHist.innerText = scoreHist.innerText + level[i].innerText + ":"
+    var myStorageScore = localStorage.getItem(category + "_" + level[i].innerText);
+    scoreHist.innerText = scoreHist.innerText + myStorageScore + "\n"
+}
+
